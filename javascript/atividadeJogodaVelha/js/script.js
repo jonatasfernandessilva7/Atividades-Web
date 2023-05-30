0f0e29387733c519daa525c75f2908d709e9d2a5
jonatasfernandessilva7 <jonatasfernandes@alu.ufc.br>
2023-05-30 15:22:03 -0300

    atividade jogo pedra papel tesoura

--- javascript/atividadeJogodaVelha/js/script.js
+++ javascript/atividadeJogodaVelha/js/script.js
@@ -1,45 +1,66 @@
-let botaoIniciarJogo = document.getElementById('jogar');
-let papel = document.getElementById('papel');
-let pedra = document.getElementById('pedra');
-let tesoura = document.getElementById('tesoura');
+let resultado = document.getElementById("resultado");
+let jogarNovamente = document.getElementById("jogarDeNovo");
 
+const jogadasPlayer = [
+    papel = document.getElementById('papel'),
+    pedra = document.getElementById('pedra'),
+    tesoura = document.getElementById('tesoura')
+]
+
+const imagensDaIA = [
+    "<img src='./img/papel.png'>",
+    "<img src='./img/tesoura.png'>",
+    "<img src='./img/pedra.png'>"
+];
+
+const ramdonImages = Math.floor(Math.random() * imagensDaIA.length);
+
+function sucesso() {
+    let msg = document.getElementById('msg');
+    msg.innerHTML = "<strong class='bg-success'>escolha feita com sucesso</strong>";
+
+
+    var imagens = imagensDaIA[ramdonImages];
+    let escolha = document.getElementById('escolhaDaIA');
+    escolha.innerHTML = ' A ia escolheu: ' + imagens;
+
+}
+
+function verificaResultado(empate, derrota){
+    if (ramdonImages == empate){
+        resultado.innerHTML = "<strong class='bg-warning'>EMPATE</strong>"
+    }else if(ramdonImages == derrota) {
+        resultado.innerHTML = "<strong class='bg-danger'>DERROTA</strong>"
+    }else{
+        resultado.innerHTML = "<strong class='bg-success'>VITORIA</strong>"
+    }
+}
+
+function reiniciar() {
+    window.location.href = 'PedraPapelTesoura.html';
+}
 
 papel.addEventListener('click', () => {
     pedra.hidden = true;
     tesoura.hidden = true;
+    sucesso();
+    verificaResultado(0,1);
 });
 
 pedra.addEventListener('click', () => {
     papel.hidden = true;
     tesoura.hidden = true;
+    sucesso();
+    verificaResultado(2,0);
 });
 
 tesoura.addEventListener('click', () => {
     pedra.hidden = true;
     papel.hidden = true;
+    sucesso();
+    verificaResultado(1,2);
 });
 
-botaoIniciarJogo.addEventListener('click', () => {
-
-    if (!papel.hidden) {
-        sucesso();
-    } else if (!pedra.hidden) {
-        sucesso();
-    } else if (!tesoura.hidden) {
-        sucesso();
-    }
-
-    if(!pedra.hidden && !papel.hidden && !tesoura.hidden){
-        erro();
-    }
+jogarNovamente.addEventListener("click", () => {
+    reiniciar();
 });
-
-function erro() {
-    let msg = document.getElementById('msg');
-    msg.innerHTML = "<strong class='bg-danger'>você não escolheu nada, escolha novamente</strong>";
-}
-
-function sucesso() {
-    let msg = document.getElementById('msg');
-    msg.innerHTML = "<strong class='bg-success'>escolha feita com sucesso</strong>";
-}
